54971a20a5f35b975f3b12dfa41523df9b649c6e
Ruchitha Yeravelli <yeravelliruchitha@gmail.com>
2025-09-01 13:00:11 +0530

    normal code

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
 print("hai-topuc")
+print('01.09.2025')
+# list1
+k =  3
+list1 = [1, 2, 3, 4, 5, 6, 7]
+print(list[0 : 3])
+print(list1[3 : ])
+list1 = list1[3 : ] + list1[0: k]
+print(list1)
+# Lambda functions
